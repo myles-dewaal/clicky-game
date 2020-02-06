7d654c2ada2b529960bc92d6352604df02689f70
Myles Dewaal <52334937+myles-dewaal@users.noreply.github.com>
2020-02-05 21:30:13 -0700

    Updates

---
+++ precache-manifest.558414a75e2f6784020e7a8c76133b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6639e949c62ae718e15146b58a1a17d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "806add024ab1671742ff",
+    "url": "/clicky-game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "24174d323ac21f04f040",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.49212cf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "806add024ab1671742ff",
+    "url": "/clicky-game/static/js/main.3e1a61e1.chunk.js"
+  },
+  {
+    "revision": "0f9f9acf91bc4b9d055b",
+    "url": "/clicky-game/static/js/runtime-main.c289e6cb.js"
+  }
+]);
